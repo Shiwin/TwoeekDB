31c82cb792a83c1dfb2521b61a702b4b0da35bc1
Ivan_Sharavuev <shpiwan@gmail.com>
2015-11-10 11:12:35 +0300

    new file:   test/StructuredFileTest.java

---
+++ test/StructuredFileTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert.*;
+
+/**
+ * Created by ivan on 10.11.15.
+ */
+public class StructuredFileTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateStructuredFile() throws Exception {
+
+    }
+}
