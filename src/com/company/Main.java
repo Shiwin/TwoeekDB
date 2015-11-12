f2c341a85ce0ea071ef5f69fb309994ea84ad59e
Ivan_Sharavuev <shpiwan@gmail.com>
2015-11-12 18:01:39 +0300

    modified:   src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,35 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         String fileName = "test.txt";
-        File file = new File(fileName);
+        DB db = new DB(fileName);
 
-        StructuredFileBlock fileBlocks = null;
-        RandomAccessFile raf;
-        long[] blocksPositions = new long[]{100,1000,3000,7000,10000};
-        try {
-            if (file.exists()) {
-                raf = new RandomAccessFile(file, "rw");
-
-                long[] lastSubBlocks = new long[]{10200, 12600};
-
-                //fileBlocks = new StructuredFileBlock(raf,10000, 15000);
-                fileBlocks = StructuredFileBlock.createStructuredFileBlock(raf, 10000, 15000, lastSubBlocks);
-            }else {
-                file.createNewFile();
-                raf = new RandomAccessFile(file, "rw");
-                raf.setLength(15000);
-
-                fileBlocks = StructuredFileBlock.createStructuredFileBlock(raf, 0, raf.length(), blocksPositions);
-            }
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
-        if(fileBlocks == null){
-            System.out.println("lol");
-            return;
-        }
-
-        System.out.println(fileBlocks.getCountOfBlocks());
     }
 }
