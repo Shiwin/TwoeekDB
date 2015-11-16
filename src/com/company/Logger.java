7b02e925bf9d286e73106c97c1895dbadde036f6
Sergey Koltsov <by3dring@gmail.com>
2015-11-16 19:58:28 +0500

    Logger is added

---
+++ src/com/company/Logger.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Сергей on 16.11.2015.
+ */
+public class Logger {
+
+    public static void message(String text){
+        System.out.println(text);
+    }
+
+}
