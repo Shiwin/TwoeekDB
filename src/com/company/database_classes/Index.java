2c63632d9c951b8116b6800557a4ad2262f87d5b
Sergey Koltsov <by3dring@gmail.com>
2015-11-21 15:11:35 +0500

    Logic of indexes have moved to class Index (instead of PrimaryIndex)

--- src/com/company/database_classes/Index.java
+++ src/com/company/database_classes/Index.java
@@ -1,4 +1,35 @@
 package com.company.database_classes;
 
+import com.company.file_access.FileHashMap;
+import com.company.file_access.TableAccess;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Index {
+    protected FileHashMap map;    //file hashmap
+
+    Index(RandomAccessFile raf, long startPosition, long endPosition, TableAccess tableAccessor, int columnNumber, int sizeOfValue, int countOfValues, boolean shouldBeInit) throws IOException {
+        this.map = new FileHashMap(raf, startPosition, endPosition,tableAccessor.getMaxTableSize(),
+                tableAccessor.getColumnSize(columnNumber), sizeOfValue,countOfValues,true);
+        if(shouldBeInit){
+            this.map.initializeEmpty();
+        }
+    }
+
+    public boolean put(String key, int value) throws Exception {
+        return this.map.put(key, value);
+    }
+
+    public int[] get(String key) throws Exception {
+        return this.map.get(key);
+    }
+
+    public int getSizeOfKey(){
+        return this.map.getSizeOfKey();
+    }
+
+    public int getSizeOfValue(){
+        return this.map.getSizeOfValue();
+    }
 }
